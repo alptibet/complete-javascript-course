766b437efe9996c2c4195c864fd5a22482281c26
Alp Tibet <a.tibet@yandex.ru>
2021-03-06 11:35:49 +0300

    commit

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -60,7 +60,6 @@ const labelSumIn = document.querySelector('.summary__value--in');
 const labelSumOut = document.querySelector('.summary__value--out');
 const labelSumInterest = document.querySelector('.summary__value--interest');
 const labelTimer = document.querySelector('.timer');
-
 const containerApp = document.querySelector('.app');
 const containerMovements = document.querySelector('.movements');
 
